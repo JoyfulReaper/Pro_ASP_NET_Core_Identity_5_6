7d409e8fb3566081c4e4105cacac4b7d58e3d839
Kyle Givler <xvegan88x@gmail.com>
2022-06-12 21:38:33 -0400

    Chapter 4: Using the identity UI Package

--- IdentityApp/IdentityApp/Program.cs
+++ IdentityApp/IdentityApp/Program.cs
@@ -1,4 +1,8 @@
 using IdentityApp.Models;
+using IdentityApp.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-// Start add EntityFramework Core For Identity
+// Start add EntityFramework Core
 var connectionString = builder.Configuration.GetConnectionString("AppDataConnection");
 builder.Services.AddDbContext<ProductDbContext>(opts =>
 {
@@ -14,8 +18,24 @@ builder.Services.AddDbContext<ProductDbContext>(opts =>
     .EnableSensitiveDataLogging();
 });
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// End add EntityFramework Core
+
+// Start add EntityFramework Core For Identity
+builder.Services.AddDbContext<IdentityDbContext>(opts =>
+{
+    opts.UseSqlServer(connectionString, 
+        opts => opts.MigrationsAssembly("IdentityApp"))
+    .EnableSensitiveDataLogging();
+});
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 // End add EntityFramework Core For Identity
 
+builder.Services.AddScoped<IEmailSender, ConsoleEmailSender>();
+
+// Start add Identity
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<IdentityDbContext>();
+// End add Identity
 
 var app = builder.Build();
 
