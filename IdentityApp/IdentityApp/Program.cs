37719a120e32f93d600d80f52e3a325f7cbe43a3
Kyle Givler <xvegan88x@gmail.com>
2022-06-13 20:50:56 -0400

    Chapter 6: Adapting Identity UI

--- IdentityApp/IdentityApp/Program.cs
+++ IdentityApp/IdentityApp/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
